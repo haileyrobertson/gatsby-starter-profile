fee19691b3e3e66fdb5a3ba5825d2a82c3f4eb83
haileyrobertson <65562768+haileyrobertson@users.noreply.github.com>
2023-12-28 17:16:12 -0600

    Format pubs

--- src/components/publications/publications.js
+++ src/components/publications/publications.js
@@ -15,7 +15,22 @@ const Publications = ({ publications }) => (
       <header>
         <h1>Publications in Review</h1>
       </header>
-      <h6>List</h6>
+      <ul>
+        <li>
+          Moore M, <strong>Robertson H</strong>, Rosado D, Graeden E, Carlson C,
+          and Katz R.{" "}
+          <strong>
+            Preparedness in practice: An outbreak science approach to studying
+            public health emergency response.
+          </strong>
+          <em> medXriv.</em> 2023, DOI:{" "}
+          <a href="https://doi.org/10.1101/2023.06.24.23291861">
+            10.1038/s41597-023-02398-3
+          </a>
+          .
+        </li>
+        <li>Your second publication in review</li>
+      </ul>
     </section>
     <section>
       <header>
